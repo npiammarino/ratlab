b47e11efb39d3466b3c27818361be868cce1878c
nick.iammarino <nickiammarino@gmail.com>
2024-02-25 13:25:11 -0500

    added custome printing for vectors

---
+++ src/linear.rs
@@ -0,0 +1,118 @@
+use std::fmt;
+
+#[derive(Debug, Clone)]
+pub struct VectorError;
+impl fmt::Display for VectorError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "VECTOR ERROR, SON!")
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Direction {
+    Row,
+    Column,
+}
+use crate::Direction::{Column, Row};
+
+pub trait Number: ToString + fmt::Display + fmt::Debug {}
+impl<T> Number for T where T: ToString + fmt::Display + fmt::Debug {}
+
+#[derive(Debug, Clone)]
+pub struct Vector<T: Number> {
+    values: Option<Vec<T>>,
+    direction: Option<Direction>,
+    element_type: String,
+}
+
+impl<T: Number> fmt::Display for Vector<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match (&self.direction, &self.values) {
+            (Some(dir), Some(_values)) => match dir {
+                // these need adjustments for length
+                Column => {
+                    let values = format!("{:?}", self.values.as_ref().unwrap());
+                    let column: String = values
+                        .split(" ")
+                        .collect::<Vec<&str>>()
+                        .join("\n   ")
+                        .replace(&['[', ']'], "");
+
+                    write!(f, "<{}>\n[  {}  ]", self.element_type, column)
+                }
+                Row => {
+                    write!(
+                        f,
+                        "<{}> {:?}",
+                        self.element_type,
+                        self.values.as_ref().unwrap()
+                    )
+                }
+            },
+            _ => {
+                if let Some(dir) = &self.direction {
+                    write!(f, "<{}> {dir:?} []", self.element_type)
+                } else if let Some(vals) = &self.values {
+                    write!(f, "<{}> Undirected {vals:?}", self.element_type)
+                } else {
+                    write!(f, "<{}> Undirected []", self.element_type)
+                }
+            }
+        }
+    }
+}
+
+impl<T: Number> Vector<T> {
+    pub fn new() -> Vector<T> {
+        Vector {
+            values: None,
+            direction: None,
+            element_type: String::from(std::any::type_name::<T>()),
+        }
+    }
+
+    pub fn build(values: Vec<T>, direction: Direction) -> Vector<T> {
+        Vector {
+            values: Some(values),
+            direction: Some(direction),
+            element_type: String::from(std::any::type_name::<T>()),
+        }
+    }
+
+    pub fn populate(&mut self, values: Vec<T>) -> Result<(), VectorError> {
+        match self.values {
+            None => {
+                self.values = Some(values);
+                Ok(())
+            }
+            _ => Err(VectorError),
+        }
+    }
+
+    pub fn direct(&mut self, direction: Direction) {
+        self.direction = Some(direction);
+    }
+}
+
+fn main() {
+    let mut x = Vector::<i32>::new();
+    let values = vec![-3, 5, 21];
+    x.populate(values).unwrap();
+    x.direct(Column);
+
+    let y = Vector::build(vec![4u8, 3, 7], Row);
+    let z = Vector::<f64>::new();
+
+    let mut no_dir = Vector::<usize>::new();
+    no_dir.populate(vec![3, 9, 8]).unwrap();
+
+    let mut no_vals = Vector::<i8>::new();
+    no_vals.direct(Row);
+
+    println!("x = {x}");
+    println!("y = {y}");
+    println!("z = {z}");
+
+    println!("no_dir = {no_dir}");
+    println!("no_vals = {no_vals}");
+}
