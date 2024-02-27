6446fa0900f2e928bd08bf3d278e7376e6273388
nick.iammarino <nickiammarino@gmail.com>
2024-02-26 21:50:53 -0500

    added test for vector + and vector eq

--- test/unit/test-linear.rs
+++ test/unit/test-linear.rs
@@ -16,3 +16,38 @@ fn test_inits_vector() {
     w.populate(vec![1, 2, 3]).unwrap();
     assert_eq!(format!("{w}"), "<usize> Undirected [1, 2, 3]");
 }
+
+#[test]
+fn test_eq() {
+    let t1 = Vector::<i32>::build(vec![4, 4, 4], Row);
+
+    let r1 = Vector::<i32>::build(vec![4, 4, 4], Row);
+    let r2 = Vector::<i32>::build(vec![4, 4, 4], Column);
+
+    let bad1 = Vector::<i32>::build(vec![3, 3, 3], Row);
+    let bad2 = Vector::<i32>::build(vec![4, 4], Row);
+
+    assert_eq!(t1, r1);
+    assert_eq!(t1, r2);
+
+    assert_ne!(t1, bad1);
+    assert_ne!(t1, bad2);
+}
+
+#[test]
+fn test_add() {
+    let x = Vector::<i32>::build(vec![1, 2, 3], Row);
+    let y = Vector::<i32>::build(vec![3, 2, 1], Column);
+
+    let r1 = Vector::<i32>::build(vec![4, 4, 4], Row);
+    let r2 = Vector::<i32>::build(vec![4, 4, 4], Column);
+
+    let bad1 = Vector::<i32>::build(vec![3, 3, 3], Row);
+    let bad2 = Vector::<i32>::build(vec![4, 4], Row);
+
+    assert_eq!(&x + &y, r1);
+    assert_eq!(&x + &y, r2);
+
+    assert_ne!(&x + &y, bad1);
+    assert_ne!(&x + &y, bad2);
+}
